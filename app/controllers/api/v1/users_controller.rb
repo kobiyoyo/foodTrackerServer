8dec2b585d5be9ae4381c11dfc913f8622a23a5d
kobiyoyo <adamachubi@gmail.com>
2020-07-14 01:07:13 +0100

    wrote test for all request

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
+ 
     if @user.save
       render json: @user, status: :created
     else
